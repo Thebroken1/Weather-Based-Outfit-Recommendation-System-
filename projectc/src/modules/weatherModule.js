affa871df501f40c7bd8e6b568f56c7d128c13c5
VS <vaish261@gmail.com>
2024-10-28 10:56:12 +0400

    Adding Weather Module

---
+++ projectc/src/modules/weatherModule.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function weatherModule(){
+    return(
+        <div className = "weatherMod">
+            <p>Weather</p>
+        </div>
+    )
+}
+export default weatherModule;
